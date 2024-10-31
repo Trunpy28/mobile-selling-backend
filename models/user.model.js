ea740bb02fb224cb180782e57b7c8d44245c75c4
Trunpy28 <127753383+Trunpy28@users.noreply.github.com>
2024-10-31 10:18:00 +0700

    Add user model

---
+++ models/user.model.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    name: {
+        type: String,
+        default: '',
+    },
+    phoneNumber: {
+        type: String,
+        default: ''
+    },
+    address: {
+        type: String,
+        default: ''
+    },
+    role: {
+        type: String,
+        default: 'User',
+        enum: ["User", "Admin"]
+    }
+}, {
+    timestamps: true
+});
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
