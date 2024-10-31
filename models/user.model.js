83ab8110dd571148b3e29436c0c9ddde58110807
tunguyen02 <tu700131@gmail.com>
2024-10-31 16:08:55 +0700

    modify userModel

--- models/user.model.js
+++ models/user.model.js
@@ -1,15 +1,27 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const userSchema = new mongoose.Schema({
     email: {
         type: String,
-        required: true,
+        required: [true, 'Please provide your email'],
         unique: true,
         match: /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/
     },
     password: {
         type: String,
-        required: true,
+        required: [true, 'Please provide a password'],
+        minlength: 6
+    },
+    passwordConfirm: {
+        type: String,
+        required: [true, 'Please confirm your password'],
+        validate: {
+            validator: function (el) {
+                return el === this.password;
+            },
+            message: 'Passwords are not the same'
+        }
     },
     name: {
         type: String,
@@ -30,11 +42,24 @@ const userSchema = new mongoose.Schema({
         type: String,
         default: 'User',
         enum: ["User", "Admin"]
+    },
+    passwordChangedAt: Date
+},
+    {
+        timestamps: true
     }
-}, {
-    timestamps: true
-});
+);
+
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next();
+    this.password = await bcrypt.hash(this.password, 12);
+    this.passwordConfirm = undefined;
+    next();
+})
+
+userSchema.methods.correctPassword = async function (candidatePassword, userPassword) {
+    return await bcrypt.compare(candidatePassword, userPassword);
+}
 
 const User = mongoose.model("User", userSchema);
-
 export default User;
