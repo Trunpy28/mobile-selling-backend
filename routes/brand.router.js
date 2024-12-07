e08be0f1eccd8169f167f96b19fe4ba74160a9eb
tunguyen02 <tu700131@gmail.com>
2024-12-07 21:12:05 +0700

    edit

--- routes/brand.router.js
+++ routes/brand.router.js
@@ -12,7 +12,7 @@ BrandRouter.get('/get-all', brandController.getAllBrands);
 BrandRouter.get('/brand-by-name/:name', brandController.getBrandByName);
 BrandRouter.post('/create', upload.single('logoUrl'), brandController.createBrand);
 BrandRouter.get('/details/:id', brandController.getBrandById);
-BrandRouter.put('/update/:id', authMiddleware, upload.single("logoUrl"), brandController.updateBrand);
+BrandRouter.put('/update/:id', upload.single('logoUrl'), brandController.updateBrand);
 BrandRouter.delete('/delete/:id', authMiddleware, brandController.deleteBrand);
 
 export default BrandRouter;
